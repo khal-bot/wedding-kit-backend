f6be3739f2105f1b05164f1fd8d415e8e8dc8e41
Khalid <asliyamohammednur4@gmail.com>
2026-09-14 03:23:16 +0300

    Fix templates folder and order storage

--- storage.js
+++ storage.js
@@ -1,11 +1,25 @@
-const orders = {};
+const fs = require('fs');
+const path = require('path');
+
+const filePath = path.join(__dirname, 'orders.json');
+
+function loadOrders() {
+  if (!fs.existsSync(filePath)) {
+    return {};
+  }
+  const data = fs.readFileSync(filePath, 'utf8');
+  return JSON.parse(data);
+}
 
 function saveOrder(id, data) {
+  const orders = loadOrders();
   orders[id] = data;
+  fs.writeFileSync(filePath, JSON.stringify(orders, null, 2));
   console.log('Saved order:', id, data);
 }
 
 function getOrder(id) {
+  const orders = loadOrders();
   return orders[id];
 }
 
